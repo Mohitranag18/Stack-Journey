bf0ebe62eea071c57f15413d466b52dfe6c2500f
Mohit Rana <mohitr8998@gmail.com>
2024-12-07 08:29:51 -0500

    change db

--- stackjourney/settings.py
+++ stackjourney/settings.py
@@ -85,11 +85,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DATABASE'),
-        'USER': os.getenv('MYSQL_USER'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': os.getenv('MYSQL_HOST', 'localhost'),
-        'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'NAME': 'stackjourney',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
